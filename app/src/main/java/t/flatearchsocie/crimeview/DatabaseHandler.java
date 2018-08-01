20835294d90cffb449fbd72e01112375c83d7b40
Clint <Clintjoshuamclean@gmail.com>
2018-08-01 21:46:39 +0200

    master

--- app/src/main/java/t/flatearchsocie/crimeview/DatabaseHandler.java
+++ app/src/main/java/t/flatearchsocie/crimeview/DatabaseHandler.java
@@ -1,4 +1,18 @@
 package t.flatearchsocie.crimeview;
 
-public class DatabaseHandler {
+
+
+import java.io.Serializable;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DatabaseHandler implements Serializable {
+
+    private Connection connection;
+    private Statement statement;
+    private CallableStatement storedProcedure;
+
+    public DatabaseHandler() {
+    }
 }
--- app/src/main/java/t/flatearchsocie/crimeview/Login.java
+++ app/src/main/java/t/flatearchsocie/crimeview/Login.java
@@ -1,7 +1,7 @@
 package t.flatearchsocie.crimeview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +9,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+       /* DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer, R.string.closeDrawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+
+
+
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+            drawerToggle.setHomeAsUpIndicator(R.drawable.ic_custom_drawer_icon); // drawer closed-reset icon
+        } else {
+            //open drawer
+            drawerLayout.openDrawer(GravityCompat.START);
+            drawerToggle.setHomeAsUpIndicator(R.drawable.ic_new_icon); // set your back icon
+        }*/
     }
 }
